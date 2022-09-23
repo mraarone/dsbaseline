d75bb3e8d9d3ab8c7e20c4bfce46efafbd0b7db3
mraarone <mraarone@yahoo.com>
2022-09-23 23:20:41 +0000

    Added debian gpg keys.

--- .devcontainer/library-scripts/microsoft-gpg-keys.sh
+++ .devcontainer/library-scripts/microsoft-gpg-keys.sh
@@ -4,8 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
 
+
 # Supply Chain Security: Install VS Code for use in desktop if needed
 curl -sSL ${MICROSOFT_GPG_KEYS_URI} | gpg --dearmor > /usr/share/keyrings/microsoft-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
 apt-get update
 apt-get -y install code code-insiders
+
+# Update debian gpg keys
+for i in `sudo apt-get -qq update 2>&1 | sed 's/.*PUBKEY //'`; do sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $i; done
