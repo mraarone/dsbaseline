ff32656ff9c60273a3f64e6f506b80059cf1a016
mraarone <mraarone@yahoo.com>
2022-09-24 03:36:46 +0000

    Making correction to format

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -18,8 +18,9 @@ install_extension() {
 
 # Install VS Code extensions into VS Code in desktop so we can try
 install_extension ms-vscode-remote.remote-containers
-install_extension ms-azuretools.vscode-docker
-install_extension streetsidesoftware.code-spell-checker
 install_extension chrisdias.vscode-opennewinstance
 install_extension mads-hartmann.bash-ide-vscode
 install_extension rogalmic.bash-debug
+install_extension streetsidesoftware.code-spell-checker
+install_extension ms-azuretools.vscode-docker
+install_extension ms-kubernetes-tools.vscode-kubernetes-tools
