e98a6a5c0613b90d8272d40a92594516ba44041f
mraarone <mraarone@yahoo.com>
2022-09-24 01:06:56 -0500

    Cleaning up postCreateCommand.sh / dotfiles system

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-# 
-sleep 1000
-
 # # Setup Fluxbox menus
 # mkdir -p /root/.fluxbox /home/vscode/.fluxbox
 # cp -f /tmp/fluxbox/* /root/.fluxbox/
 # cp -f /tmp/fluxbox/* /home/vscode/.fluxbox/
 # chown -R vscode:vscode /home/vscode/.fluxbox
-[[ -z /etc/zsh/zshrc ]] && mkdir --parent /etc/zsh/zshrc && touch /etc/zsh/zshrc
-echo "export DONT_PROMPT_WSL_INSTALL=true" | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc >> /etc/profile.d/not-wsl.sh
-
-# Fluxbox: Install firefox for fluxbox
-apt-get install -y firefox-esr
 
 install_extension() {
     /usr/bin/code --install-extension $1
-    /usr/bin/code-insiders --install-extension $1
+#    /usr/bin/code-insiders --install-extension $1
 }
 
 # Install VS Code extensions into VS Code in desktop so we can try
@@ -27,3 +19,10 @@ install_extension rogalmic.bash-debug
 install_extension streetsidesoftware.code-spell-checker
 install_extension ms-azuretools.vscode-docker
 install_extension ms-kubernetes-tools.vscode-kubernetes-tools
+
+# Fluxbox: Install firefox for fluxbox
+apt-get install -y firefox-esr
+
+#[[ -z /etc/zsh/zshrc ]] && mkdir --parent /etc/zsh/zshrc && touch /etc/zsh/zshrc
+#echo "export DONT_PROMPT_WSL_INSTALL=true" | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc >> /etc/profile.d/not-wsl.sh
+
