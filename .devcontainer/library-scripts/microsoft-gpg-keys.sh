e39d4eb0a67a5ffbeb4c9efe14583e2af9913738
mraarone <mraarone@yahoo.com>
2022-09-28 03:20:04 -0500

    Removed hard installation of code/code-insider

--- .devcontainer/library-scripts/microsoft-gpg-keys.sh
+++ .devcontainer/library-scripts/microsoft-gpg-keys.sh
@@ -9,7 +9,7 @@ MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
 curl -sSL ${MICROSOFT_GPG_KEYS_URI} | gpg --dearmor > /usr/share/keyrings/microsoft-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
 apt-get update
-apt-get -y install code code-insiders
+# apt-get -y install code code-insiders
 
 # Update debian gpg keys
 for i in `apt-get -qq update 2>&1 | sed 's/.*PUBKEY //'`; do apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $i; done
