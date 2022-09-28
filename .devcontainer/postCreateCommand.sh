c1f96c31b8e82cc2bec49fd47cd0b69f83947374
mraarone <mraarone@yahoo.com>
2022-09-28 08:43:01 +0000

    Removed explicit vscode install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,23 +7,23 @@
 # chown -R vscode:vscode /home/vscode/.fluxbox
 
 # Fluxbox: Install firefox for fluxbox
-apt-get install -y firefox-esr
+#apt-get install -y firefox-esr
 
 setopt CLOBBER
 [[ -z /etc/zsh/zshrc ]] && mkdir --parent /etc/zsh/zshrc && touch /etc/zsh/zshrc
 echo "export DONT_PROMPT_WSL_INSTALL=true" | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc >> /etc/profile.d/not-wsl.sh
 
-install_extension() {
-    /usr/bin/code --install-extension $1
+#install_extension() {
+#    /usr/bin/code --install-extension $1
 #    /usr/bin/code-insiders --install-extension $1
-}
+#}
 
 # Install VS Code extensions into VS Code in desktop so we can try
-install_extension ms-vscode-remote.remote-containers
-install_extension chrisdias.vscode-opennewinstance
-install_extension mads-hartmann.bash-ide-vscode
-install_extension rogalmic.bash-debug
-install_extension streetsidesoftware.code-spell-checker
-install_extension ms-azuretools.vscode-docker
-install_extension ms-kubernetes-tools.vscode-kubernetes-tools
+#install_extension ms-vscode-remote.remote-containers
+#install_extension chrisdias.vscode-opennewinstance
+#install_extension mads-hartmann.bash-ide-vscode
+#install_extension rogalmic.bash-debug
+#install_extension streetsidesoftware.code-spell-checker
+#install_extension ms-azuretools.vscode-docker
+#install_extension ms-kubernetes-tools.vscode-kubernetes-tools
 
