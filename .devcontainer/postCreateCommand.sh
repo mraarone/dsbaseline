d583fd6187e2b59fff4f27a636c6ac61a7c4ab44
Aaron Elliott <mraarone@yahoo.com>
2022-09-28 21:21:49 -0500

    Moving where extension installers find code and insiders

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,17 +6,14 @@
 #  cp -f /tmp/fluxbox/* /home/vscode/.fluxbox/
 #  chown -R node:node /home/node/.fluxbox
 
-# Fluxbox: Install firefox for fluxbox
-apt-get install -y firefox-esr
-
 #setopt CLOBBER # Works in zshell (zsh), makes >> pipe create a file if it doesn't already exist
 # This, below, however does not work in anything except zshell.
 #[[ -z /etc/zsh/zshrc ]] && mkdir --parent /etc/zsh/zshrc && touch /etc/zsh/zshrc
 #echo "export DONT_PROMPT_WSL_INSTALL=true" | tee -a /etc/bash.bashrc >> /etc/profile.d/not-wsl.sh >> /etc/zsh/zshrc
 
 install_extension() {
-    /usr/bin/code --install-extension $1
-    /usr/bin/code-insiders --install-extension $1
+    /usr/local/bin/code --install-extension $1
+    /usr/local/bin/code-insiders --install-extension $1
 }
 
 # Install VS Code extensions into VS Code in desktop so we can try
@@ -27,4 +24,11 @@ install_extension rogalmic.bash-debug
 install_extension streetsidesoftware.code-spell-checker
 install_extension ms-azuretools.vscode-docker
 install_extension ms-kubernetes-tools.vscode-kubernetes-tools
-
+install_extension ms-python.python
+install_extension ms-python.vscode-pylance
+install_extension mechatroner.rainbow-csv
+install_extension oderwat.indent-rainbow
+install_extension SonarSource.sonarlint-vscode
+install_extension ms-vscode.makefile-tools
+install_extension redhat.vscode-yaml
+install_extension GitHub.copilot
