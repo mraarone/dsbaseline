2ec8bff6110fdf4d1dc07fd20507da01929f294f
Aaron Elliott <mraarone@yahoo.com>
2022-09-29 16:54:34 -0500

    Sync'd extensions from container to local recommends

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,22 +13,32 @@
 
 install_extension() {
     /usr/local/bin/code --install-extension $1
-#    /usr/local/bin/code-insiders --install-extension $1
+    /usr/bin/code-insiders --install-extension $1
 }
 
 # Install VS Code extensions into VS Code in desktop so we can try
-install_extension ms-vscode-remote.remote-containers
-install_extension chrisdias.vscode-opennewinstance
-install_extension mads-hartmann.bash-ide-vscode
 install_extension rogalmic.bash-debug
+install_extension mads-hartmann.bash-ide-vscode
 install_extension streetsidesoftware.code-spell-checker
 install_extension ms-azuretools.vscode-docker
-install_extension ms-kubernetes-tools.vscode-kubernetes-tools
-install_extension ms-python.python
-install_extension ms-python.vscode-pylance
-install_extension mechatroner.rainbow-csv
-install_extension oderwat.indent-rainbow
-install_extension SonarSource.sonarlint-vscode
-install_extension ms-vscode.makefile-tools
-install_extension redhat.vscode-yaml
 install_extension GitHub.copilot
+install_extension oderwat.indent-rainbow
+install_extension ms-toolsai.jupyter
+install_extension ms-toolsai.jupyter-renderers
+install_extension ms-kubernetes-tools.vscode-kubernetes-tools
+install_extension ms-vscode.makefile-tools
+install_extension chrisdias.vscode-opennewinstance
+install_extension ms-python.vscode-pylance
+install_extension ms-python.python
+install_extension mechatroner.rainbow-csv
+install_extension SonarSource.sonarlint-vscode
+install_extension redhat.vscode-yaml
+install_extension ms-vscode-remote.remote-containers
+install_extension eamodio.gitlens
+install_extension JHeilingbrunner.vscode-gnupg-tool
+install_extension hashicorp.hcl
+install_extension bierner.markdown-mermaid
+install_extension DavidAnson.vscode-markdownlint
+install_extension mintlify.document
+install_extension luisfontes19.vscode-swissknife
+install_extension streetsidesoftware.code-spell-checker
